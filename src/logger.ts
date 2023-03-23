8a684ca977118451f8dd2bcdb8d6003644d14071
talhasch <talhabugrabulut@gmail.com>
2023-03-23 14:45:49 +0300

    eslint no-console

--- src/logger.ts
+++ src/logger.ts
@@ -1,6 +1,7 @@
 export const createLogger = (name: string) => {
     return {
         info: (message?: any, ...optionalParams: any[]) => {
+            // eslint-disable-next-line
             console.log(name, message, ...optionalParams);
         }
     }
