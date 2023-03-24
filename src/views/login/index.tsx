4a04d7a90c71e82a02c9f254e137eee514101368
talhasch <talhabugrabulut@gmail.com>
2023-03-24 12:48:00 +0300

    updated github link

--- src/views/login/index.tsx
+++ src/views/login/index.tsx
@@ -129,7 +129,7 @@ const LoginPage = (_: RouteComponentProps) => {
                 </Button>
             </Card>
             <Box sx={{mt: '20px', fontSize: '0.8em', display: 'flex', justifyContent: 'center'}}>
-                <Box component="a" href="https://github.com/nostr-chat/nostr-chat" target="_blank" rel="noreferrer"
+                <Box component="a" href="https://github.com/NostrChat/NostrChat" target="_blank" rel="noreferrer"
                      sx={{display: 'inline-flex', alignItems: 'center', color: theme.palette.text.secondary}}>
                     <Github height={20} style={{marginRight: '4px'}}/> {'Github'}
                 </Box>
