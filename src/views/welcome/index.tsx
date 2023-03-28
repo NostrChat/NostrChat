e289fe4038fbea877004feab4551d0066943d86e
talhasch <talhabugrabulut@gmail.com>
2023-03-28 17:55:54 +0300

    welcome page improvements

--- src/views/welcome/index.tsx
+++ src/views/welcome/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {RouteComponentProps, useNavigate} from '@reach/router';
 import {Helmet} from 'react-helmet';
 import {useAtom} from 'jotai';
@@ -6,12 +6,11 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
 import useMediaBreakPoint from 'hooks/use-media-break-point';
 import useTranslation from 'hooks/use-translation';
 import MetadataForm from 'views/components/metadata-form';
 import {keysAtom, profileAtom, ravenAtom} from 'store';
-import Plus from 'svg/plus';
-import Close from 'svg/close';
 
 const WelcomePage = (_: RouteComponentProps) => {
     const [isSm,] = useMediaBreakPoint();
@@ -20,6 +19,7 @@ const WelcomePage = (_: RouteComponentProps) => {
     const [profile] = useAtom(profileAtom);
     const [raven] = useAtom(ravenAtom);
     const [keys] = useAtom(keysAtom);
+    const [loading, setLoading] = useState(true);
 
     useEffect(() => {
         if (profile) {
@@ -33,6 +33,12 @@ const WelcomePage = (_: RouteComponentProps) => {
         }
     }, [keys]);
 
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, [])
+
     const skip = () => {
         navigate('/').then();
     }
@@ -45,23 +51,27 @@ const WelcomePage = (_: RouteComponentProps) => {
             <Card sx={{
                 p: '26px 32px 26px 32px',
             }}>
-                <Plus width={44}/><Close width={44} style={{marginLeft: '-14px'}}/>
-                <Box sx={{
-                    fontFamily: 'Faktum, sans-serif',
-                    fontWeight: 'bold',
-                    fontSize: '2.5em',
-                    textAlign: 'center'
-                }}>
-                    {isSm ? t('Welcome to NostrChat') : t('Welcome')}
-                </Box>
-                <Divider sx={{m: '28px 0'}}/>
-                <Box sx={{color: 'text.secondary', mb: '28px'}}>{t('Setup your profile')}</Box>
-                <MetadataForm
-                    skipButton={<Button onClick={skip}>{t('Skip')}</Button>}
-                    submitBtnLabel={t('Next')}
-                    onSubmit={(data) => {
-                        raven?.updateProfile(data);
-                    }}/>
+                {(() => {
+                    if (loading) {
+                        return <Box sx={{display: 'flex', justifyContent: 'center'}}><CircularProgress/></Box>
+                    }
+
+                    return <>
+                        <Box component="img" src="/logo-large-white.png" sx={{
+                            width: isSm ? '526px' : '100%',
+                            height: isSm ? '132px' : null,
+                            m: '20px 0 10px 0'
+                        }}/>
+                        <Divider sx={{m: '28px 0'}}/>
+                        <Box sx={{color: 'text.secondary', mb: '28px'}}>{t('Setup your profile')}</Box>
+                        <MetadataForm
+                            skipButton={<Button onClick={skip}>{t('Skip')}</Button>}
+                            submitBtnLabel={t('Next')}
+                            onSubmit={(data) => {
+                                raven?.updateProfile(data);
+                            }}/>
+                    </>
+                })()}
             </Card>
         </Box>
     </>;
