ba355f44846448aff1bf1a49b76f7a3c33012dab
talhasch <talhabugrabulut@gmail.com>
2023-04-11 17:21:27 +0300

    style hooks to reuse common code

---
+++ src/hooks/use-styles.ts
@@ -0,0 +1,11 @@
+const useStyles = () => {
+    return {
+        ellipsis: {
+            overflow: 'hidden',
+            whiteSpace: 'nowrap',
+            textOverflow: 'ellipsis'
+        }
+    }
+}
+
+export default useStyles;
