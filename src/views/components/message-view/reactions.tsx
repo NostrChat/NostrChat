47bc08e5421fc61438ab4f74c0406a2b8c24ed40
talhasch <talhabugrabulut@gmail.com>
2023-04-15 14:54:52 +0300

    reaction list improvements

--- src/views/components/message-view/reactions.tsx
+++ src/views/components/message-view/reactions.tsx
@@ -1,11 +1,11 @@
-import {Message} from 'types';
+import {Message, Reaction} from 'types';
 import {useMemo} from 'react';
 import Box from '@mui/material/Box';
 import {useTheme} from '@mui/material/styles';
+import {blue} from '@mui/material/colors';
 import uniq from 'lodash.uniq';
 import {useAtom} from 'jotai';
 import {keysAtom, ravenAtom} from 'store';
-import {blue} from '@mui/material/colors';
 
 const MessageReactions = (props: { message: Message }) => {
     const {message} = props;
@@ -13,23 +13,39 @@ const MessageReactions = (props: { message: Message }) => {
     const [keys] = useAtom(keysAtom);
     const [raven] = useAtom(ravenAtom);
 
-    const reactionList = useMemo(() => message.reactions ? uniq(message.reactions.sort((a, b) => a.created - b.created).map(r => r.content)) : [], [message.reactions]);
+    const reactions: {
+        symbol: string,
+        authors: string[],
+        count: number,
+        userReaction: Reaction | undefined
+    }[] | null = useMemo(() => {
+        if (!message.reactions) return null;
 
-    if (!message.reactions) return null;
+        const symbols: Record<string, number> = {};
+        message.reactions.sort((a, b) => a.created - b.created).forEach(r => {
+            symbols[r.content] = 1;
+        });
+
+        return Object.keys(symbols).map((symbol) => {
+            const authors = uniq(message.reactions!.filter(x => x.content === symbol).map(x => x.creator));
+            const count = authors.length;
+            const userReaction = message.reactions!.find(x => x.content === symbol && x.creator === keys?.pub);
+
+            return {symbol, authors, count, userReaction}
+        });
+    }, [message.reactions]);
+
+    if (!reactions) return null;
 
     return <Box sx={{
         display: 'flex',
         mb: '4px'
-    }}>{reactionList.map(r => {
-        const authors = uniq(message.reactions!.filter(x => x.content === r).map(x => x.creator));
-        const count = authors.length;
-        const reacted = message.reactions!.find(x => x.content === r && x.creator === keys?.pub);
-
-        return <Box sx={{
+    }}>{reactions.map((r, i) => {
+        return <Box key={r.symbol} sx={{
             fontSize: '0.8em',
             p: '0 6px',
-            mr: '4px',
-            background: reacted ? blue[800] : theme.palette.background.paper,
+            mr: i === reactions.length - 1 ? null : '4px',
+            background: r.userReaction ? blue[800] : theme.palette.background.paper,
             borderRadius: '8px',
             display: 'flex',
             alignItems: 'center',
@@ -39,21 +55,19 @@ const MessageReactions = (props: { message: Message }) => {
                 borderColor: theme.palette.divider,
             },
         }} onClick={() => {
-            if(reacted){
-                raven?.deleteEvents([reacted.id]);
+            if (r.userReaction) {
+                raven?.deleteEvents([r.userReaction.id]);
                 console.log('deleteEvents')
             } else {
-                raven?.sendReaction(message.id, message.creator, r);
+                raven?.sendReaction(message.id, message.creator, r.symbol);
                 console.log('sendReaction');
             }
         }}>
-            <Box sx={{mr: '3px'}}>
-                {r}
-            </Box>
+            <Box sx={{mr: '3px'}}>{r.symbol}</Box>
             <Box sx={{
                 color: theme.palette.text.disabled,
                 fontSize: '0.9em',
-            }}>{count}</Box>
+            }}>{r.count}</Box>
         </Box>
     })}</Box>;
 }
