186e31572dba51fa9adbe2dbae085dbd8f121c6f
talhasch <talhabugrabulut@gmail.com>
2023-04-26 11:43:28 +0300

    code tweak

--- src/views/components/dialogs/login/index.tsx
+++ src/views/components/dialogs/login/index.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
 import DialogTitle from '@mui/material/DialogTitle';
-
 import DialogContent from '@mui/material/DialogContent';
+
 import CloseModal from 'components/close-modal';
 import Login from 'views/components/login';
 import useModal from 'hooks/use-modal';
-import useTranslation from 'hooks/use-translation';
 
 const LoginDialog = (props: { onLogin: () => void }) => {
     const {onLogin} = props;
     const [, showModal] = useModal();
-    const [t] = useTranslation();
 
-    const handleClose = () => {
-        showModal(null);
-    };
+    const handleClose = () => showModal(null);
 
     return (
         <>
             <DialogTitle><CloseModal onClick={handleClose}/></DialogTitle>
-            <DialogContent sx={{pb: '0'}}>
+            <DialogContent>
                 <Login onLogin={onLogin}/>
             </DialogContent>
         </>
