0ffbdf1f09fa5fa96815431953d44817f1b7cd84
talhasch <talhabugrabulut@gmail.com>
2023-05-25 17:14:01 +0300

    add tiptap history extension

--- src/views/components/chat-input/editor/index.tsx
+++ src/views/components/chat-input/editor/index.tsx
@@ -3,6 +3,7 @@ import {Extension, ReactRenderer, useEditor} from '@tiptap/react';
 import Document from '@tiptap/extension-document';
 import Paragraph from '@tiptap/extension-paragraph';
 import Text from '@tiptap/extension-text';
+import History from '@tiptap/extension-history';
 import {Mention} from '@tiptap/extension-mention';
 import tippy, {GetReferenceClientRect, Instance} from 'tippy.js';
 import {SuggestionProps} from '@tiptap/suggestion';
@@ -18,6 +19,7 @@ const useMakeEditor = ({content, onUpdate}: { content: string, onUpdate: () => v
             Document,
             Paragraph,
             Text,
+            History,
             Extension.create({
                 name: 'ShiftEnterExtension',
                 addKeyboardShortcuts() {
