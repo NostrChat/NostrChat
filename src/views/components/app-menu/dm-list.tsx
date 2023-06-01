b3c50bebc0d021e9f45d27bf714273744250df32
talhasch <talhabugrabulut@gmail.com>
2023-06-01 18:44:40 +0300

    remove list margins

--- src/views/components/app-menu/dm-list.tsx
+++ src/views/components/app-menu/dm-list.tsx
@@ -49,24 +49,22 @@ const DmList = () => {
             </Box>
             <Button onClick={search} sx={{minWidth: 'auto'}}><Plus height={18}/></Button>
         </Box>
-        <Box sx={{mt: '10px'}}>
-            {(() => {
-                if (directContacts.length === 0) {
-                    return <Box component='span' sx={{
-                        color: theme.palette.primary.dark,
-                        fontSize: '85%',
-                        opacity: '0.6',
-                    }}>{t('No direct message')}</Box>
-                }
+        {(() => {
+            if (directContacts.length === 0) {
+                return <Box component='span' sx={{
+                    color: theme.palette.primary.dark,
+                    fontSize: '85%',
+                    opacity: '0.6',
+                }}>{t('No direct message')}</Box>
+            }
 
-                return directContacts.map(p => {
-                    const profile = profiles.find(x => x.creator === p.pub);
-                    const label = profile?.name || truncateMiddle(p.npub, 28, ':');
-                    const isSelected = p.pub === directMessage && location.pathname.startsWith('/dm/');
-                    return <ListItem key={p.npub} label={label} href={`/dm/${p.npub}`} selected={isSelected}/>
-                })
-            })()}
-        </Box>
+            return directContacts.map(p => {
+                const profile = profiles.find(x => x.creator === p.pub);
+                const label = profile?.name || truncateMiddle(p.npub, 28, ':');
+                const isSelected = p.pub === directMessage && location.pathname.startsWith('/dm/');
+                return <ListItem key={p.npub} label={label} href={`/dm/${p.npub}`} selected={isSelected}/>
+            })
+        })()}
     </>
 }
 
