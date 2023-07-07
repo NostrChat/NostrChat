491983213ebe481d439f6891bf87ea3345f08923
talhasch <talhabugrabulut@gmail.com>
2023-07-07 14:09:40 +0300

    send button

--- src/views/components/dm-input/index.tsx
+++ src/views/components/dm-input/index.tsx
@@ -1,8 +1,10 @@
 import React, {useState} from 'react';
 import {useAtom} from 'jotai';
 import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 import useTranslation from 'hooks/use-translation';
 import {ravenAtom} from 'atoms';
+import Send from 'svg/send';
 
 const DmInput = (props: { pubkey: string, onDM: () => void }) => {
     const {pubkey, onDM} = props;
@@ -10,9 +12,32 @@ const DmInput = (props: { pubkey: string, onDM: () => void }) => {
     const [raven] = useAtom(ravenAtom);
     const [t] = useTranslation();
 
+    const send = () => {
+        if (message.trim() !== '') {
+            raven?.sendDirectMessage(pubkey, message).then(() => {
+                setMessage('');
+                onDM();
+            });
+        }
+    }
+
     return <TextField
         autoComplete="off"
-        InputProps={{sx: {fontSize: '14px'}}}
+        InputProps={{
+            sx: {
+                fontSize: '14px',
+                pr: '3px'
+            },
+            endAdornment: <>
+                <Button variant="contained" size="small" color="primary" sx={{
+                    minWidth: 'auto',
+                    width: '28px',
+                    height: '28px',
+                    padding: '6px',
+                    borderRadius: '4px'
+                }} onClick={send}><Send height={28}/></Button>
+            </>
+        }}
         size="small"
         fullWidth
         placeholder={t('Send direct message')}
@@ -21,11 +46,8 @@ const DmInput = (props: { pubkey: string, onDM: () => void }) => {
             setMessage(e.target.value);
         }}
         onKeyUp={(e) => {
-            if (e.key === 'Enter' && message.trim() !== '') {
-                raven?.sendDirectMessage(pubkey, message).then(() => {
-                    setMessage('');
-                    onDM();
-                });
+            if (e.key === 'Enter') {
+                send();
             }
         }}
     />
