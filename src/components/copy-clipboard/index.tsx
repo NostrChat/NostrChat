f69a67aff0ad7c23e905cc13125d98b230c08023
talhasch <talhabugrabulut@gmail.com>
2023-07-11 14:03:58 +0300

    tooltip customization for mobile

--- src/components/copy-clipboard/index.tsx
+++ src/components/copy-clipboard/index.tsx
@@ -1,10 +1,13 @@
 import React, {useMemo, useState} from 'react';
 import Tooltip from '@mui/material/Tooltip';
 import useTranslation from 'hooks/use-translation';
+import useToast from '../../hooks/use-toast';
+import {PLATFORM} from '../../const';
 
 const CopyToClipboard = (props: { children: JSX.Element, copy: string }) => {
     const [t] = useTranslation();
     const [title, setTitle] = useState(t('Copy'));
+    const [, showMessage] = useToast();
 
     const clicked = async () => {
         await navigator.clipboard?.writeText(props.copy).then();
@@ -12,6 +15,10 @@ const CopyToClipboard = (props: { children: JSX.Element, copy: string }) => {
         setTimeout(() => {
             setTitle(t('Copy'));
         }, 2000);
+
+        if (PLATFORM !== 'web') {
+            showMessage(t('Copied'), 'success', 1000);
+        }
     }
 
     const clonedChildren = useMemo(() => {
@@ -20,9 +27,13 @@ const CopyToClipboard = (props: { children: JSX.Element, copy: string }) => {
             style: {cursor: 'pointer'}
         });
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [props.children])
+    }, [props.children]);
 
-    return <Tooltip title={title} enterTouchDelay={0}>{clonedChildren}</Tooltip>;
+    if (PLATFORM !== 'web') {
+        return clonedChildren;
+    }
+
+    return <Tooltip title={title}>{clonedChildren}</Tooltip>;
 }
 
 export default CopyToClipboard;
